0cb118be162c5fc7d43fd924cff303382ce8284d
Kavya KT <150220381+KTKavya@users.noreply.github.com>
2024-12-30 14:27:07 +0530

    Add files via upload

---
+++ abc11.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public interface abc11 {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]);
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(a));
+
+        /*System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+*/
+
+    }
+}
